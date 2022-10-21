eeb7f34e12672dd7ae274415830f9fc7f4f7fac7
jiyan <mecitjiyan@gmail.com>
2022-10-21 13:51:12 -0400

    add projects.json

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+import projectsJson from './projects.json' assert {type: 'json'};
 
 
 const style = () => {
@@ -83,6 +83,101 @@ const filter = () => {
 
 }
 
+function populate_portfolio() {
+	const grid_div = document.querySelector(".grid-div");
+	let projects = projectsJson.projects;
+
+	for (let project of projects) {
+		const div = document.createElement("div");
+		div.classList.add("project-div");
+		for (let tag of project.tags) {
+			div.classList.add(tag);
+		}
+
+		const img  = document.createElement("img");
+		img.src = project['img-src'];
+		div.appendChild(img);
+
+		const name_div = document.createElement("div");
+		name_div.className = "project-name-div";
+		div.appendChild(name_div);
+
+		const name = document.createElement("span");
+		name.className = "project-name";
+		name.innerText = project.name;
+		name_div.appendChild(name);
+
+		const bottom_div = document.createElement("div")
+		bottom_div.className = "project-bottom-div";
+		div.appendChild(bottom_div);
+		
+		const info_div = document.createElement("info_div");
+		info_div.className = "project-info-div";
+		bottom_div.appendChild(info_div);
+
+		const info = document.createElement("p");
+		info.className = "project-info";
+		info.innerText = project.info;
+		info_div.appendChild(info);
+		
+		const description = document.createElement("p");
+		description.className = "project-description";
+		description.innerText = project.description;
+		bottom_div.appendChild(description);
+
+		const links_div = document.createElement("div");
+		links_div.className = "project-links-div";
+		bottom_div.append(links_div);
+
+		for (let link of project.links) {
+			const link_div = document.createElement("div");
+			link_div.className = "project-link-div";
+			links_div.appendChild(link_div);
+	
+			const link_a = document.createElement("a");
+			link_a.href = link.url;
+			link_a.target = "_blank";
+			link_div.appendChild(link_a);
+
+			const link_i = document.createElement("i");
+			link_a.appendChild(link_i);
+			
+			switch (link.to) {
+				case "github":
+					link_i.className = "fa-brands fa-github fa-2x";
+					link_div.classList.add("github");
+					console.log(link_div.className);
+					break;
+				case "youtube":
+					link_i.className = "fa-brands fa-youtube";
+					link_div.classList.add("youtube");
+					break;
+			}
+		}
+
+
+		grid_div.appendChild(div);
+	}
+}
+
 style();
 filter();
+populate_portfolio();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
