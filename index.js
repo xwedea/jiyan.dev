6a6f82c2a9af86c1958fe95d4c23cdeab1b0ae49
jiyan <mecitjiyan@gmail.com>
2022-10-21 19:53:47 -0400

    filter demo

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const style = () => {
 	const allButton = document.querySelector(".portfolio-filter-div #all");
 	allButton.style.background = "#c2c1ad";
 
+
 	// const filters = document.querySelectorAll(".portfolio-filter-div .li");
 	// for (let filter of filters) {
 	// 	filter.onmouseover = () => {
@@ -18,93 +19,114 @@ const filter = () => {
 	const school = document.querySelector(".portfolio-filter-div #school-projects");
 	const game = document.querySelector(".portfolio-filter-div #game-development");
 	const misc = document.querySelector(".portfolio-filter-div #miscellaneous");
+	const filters = [all, school, game, misc];
 
 	const projects = document.querySelectorAll(".project-div");
-	const filters = [all, school, game, misc];
 
 	all.onclick = () => {
 		for (let filter of filters) {
 			filter.style.background = "";
 		}
 		all.style.background = "#c2c1ad";
-		for (let project of projects) {
-			project.style.display = "block";
-		}
+		populate_portfolio(["school", "personal", "game"]);
+		// for (let project of projects) {
+		// 	project.style.display = "block";
+		// }
 
 	}
 
 	school.onclick = () => {
 		for (let filter of filters) {
 			filter.style.background = "";
-			console.log(filter.innerHTML);
+			// console.log(filter.innerHTML);
 		}
 		school.style.background = "#c2c1ad";
-		const school_projects = document.querySelectorAll(".project-div.school")
-		for (let project of projects) {
-			project.style.display = "none";
-		}
-		for (let project of school_projects) {
-			project.style.display = "block";
-			console.log(project.innerHTML);
-		}
+		populate_portfolio(["school"]);
+		// const school_projects = document.querySelectorAll(".project-div.school")
+		// for (let project of projects) {
+		// 	project.style.display = "none";
+		// }
+		// for (let project of school_projects) {
+		// 	project.style.display = "block";
+		// 	console.log(project.classList);
+		// }
 	}
 
 	game.onclick = () => {
 		for (let filter of filters) {
 			filter.style.background = "";
-			console.log(filter.innerHTML);
 		}
 		game.style.background = "#c2c1ad";
-		const game_projects = document.querySelectorAll(".project-div.game")
-		for (let project of projects) {
-			project.style.display = "none";
-		}
-		for (let project of game_projects) {
-			project.style.display = "block";
-			console.log(project.innerHTML);
-		}
+		populate_portfolio(["game"]);
+
+		// const game_projects = document.querySelectorAll(".project-div.game")
+		// for (let project of projects) {
+		// 	project.style.display = "none";
+		// }
+		// for (let project of game_projects) {
+		// 	project.style.display = "block";
+		// 	console.log(project.innerHTML);
+		// }
 	}
 
 	misc.onclick = () => {
 		for (let filter of filters) {
 			filter.style.background = "";
-			console.log(filter.innerHTML);
 		}
 		misc.style.background = "#c2c1ad";
-		misc_projects = document.querySelectorAll(".project-div.misc")
-		for (let project of projects) {
-			project.style.display = "none";
-		}
-		for (let project of misc_projects) {
-			project.style.display = "block";
-			console.log(project.innerHTML);
-		}
+
+		populate_portfolio(["misc"]);
+		// misc_projects = document.querySelectorAll(".project-div.misc")
+		// for (let project of projects) {
+		// 	project.style.display = "none";
+		// }
+		// for (let project of misc_projects) {
+		// 	project.style.display = "block";
+		// 	console.log(project.innerHTML);
+		// }
 	}
 
 }
 
-function populate_portfolio() {
+function populate_portfolio(tag_filters) {
 	const grid_div = document.querySelector(".grid-div");
+	grid_div.innerHTML = "";
 	let projects = projectsJson.projects;
 
 	for (let project of projects) {
+		// if (!project.tags.includes(filter)) continue;
+
+		let passFilter = false;
+		for (let filter of tag_filters) {
+			if (project.tags.includes(filter)) {
+				passFilter = true;
+				break;
+			}
+		}
+		if (!passFilter) continue;
+
 		const div = document.createElement("div");
 		div.classList.add("project-div");
 		for (let tag of project.tags) {
 			div.classList.add(tag);
+			console.log(div.className);
 		}
 
 		const top_div = document.createElement("div");
 		top_div.className = "project-top-div";
 		div.appendChild(top_div);
 
+		const label = document.createElement("label");
+		label.className = "unselectible";
+		top_div.appendChild(label);
+
 		const img  = document.createElement("img");
 		img.src = project['img-src'];
-		top_div.appendChild(img);
+		label.appendChild(img);
 
 		const name_div = document.createElement("div");
 		name_div.className = "project-name-div";
-		top_div.appendChild(name_div);
+		label.appendChild(name_div);
 
 		const name = document.createElement("span");
 		name.className = "project-name";
@@ -113,7 +135,7 @@ function populate_portfolio() {
 
 		const info_div = document.createElement("info_div");
 		info_div.className = "project-info-div";
-		top_div.appendChild(info_div);
+		label.appendChild(info_div);
 		
 		const info = document.createElement("p");
 		info.className = "project-info";
@@ -123,7 +145,7 @@ function populate_portfolio() {
 		const description = document.createElement("p");
 		description.className = "project-description";
 		description.innerText = project.description;
-		top_div.appendChild(description);
+		label.appendChild(description);
 		
 		const bottom_div = document.createElement("div")
 		bottom_div.className = "project-bottom-div";
@@ -166,7 +188,7 @@ function populate_portfolio() {
 
 style();
 filter();
-populate_portfolio();
+// populate_portfolio();
 
 
 
